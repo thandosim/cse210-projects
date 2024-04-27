5ae9c3e64abc1f664d94a8b1b6e90d54b1c2e83a
thandosim <tdmpembe@live.com>
2024-04-27 06:35:44 +0200

    prep4 instructions complete. i didnt do the stretc challenge because i used built in methods to find the sum, average and largest values

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        float number = -1;
+        List<float> numbers = new List<float>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        while (number != 0 )
+        {
+            Console.Write("Enter number: ");
+            number = float.Parse(Console.ReadLine());
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+            
+        }
+        float total = numbers.Sum();
+        float  average = numbers.Average();
+        float largest = numbers.Max();
+        numbers.ForEach(Console.WriteLine);
+        Console.WriteLine($"The sum is: {total}");
+        Console.WriteLine($"The average is: : {average}");
+        Console.WriteLine($"The largest is: : {largest}");
     }
 }
