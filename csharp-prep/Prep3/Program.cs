8e51e65d4fcd25d452c3cc763a46d28e27e48d87
thandosim <tdmpembe@live.com>
2024-04-27 05:41:44 +0200

    prep 3 instructions done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int guess=0; 
+        int magic_number;
+        int count = 0;
+        
+        Random randomGenerator = new Random();
+        magic_number = randomGenerator.Next(1,11);
+        
+        while (guess != magic_number)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            count = count + 1;
+            if (guess == magic_number)
+            {
+                Console.WriteLine("congratulations, You guessed it!");
+            }
+            else if (guess > magic_number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("Higher");
+            }
+        }
+        Console.WriteLine($"It took  you {count} guesses.");
     }
 }
