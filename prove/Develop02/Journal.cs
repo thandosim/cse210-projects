9dcb165665053c4c21c9b1364c92283d690ba37b
thandosim <tdmpembe@live.com>
2024-05-03 17:43:13 +0200

    made stub program

---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,25 @@
+public class Journal
+{
+    public List<Entry> _entries = new List<Entry>();
+
+    public void AddEntry( Entry newEntry)//revisit
+    {
+        
+    }
+
+    public void DisplayAll()
+    {
+        
+    }
+
+    public void SaveToFile( string file)
+    {
+        
+    }
+
+    public void LoadFromFile( string file)
+    {
+        
+    }
+
+}
