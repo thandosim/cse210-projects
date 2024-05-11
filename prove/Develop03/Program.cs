ccf623dd73e00434a2088916633a4efc3d4cabee
thandosim <tdmpembe@live.com>
2024-05-11 07:45:56 +0200

    removed testing number

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -8,7 +8,7 @@ class Program
     {
         Console.WriteLine("Hello Develop03 World!");
 
-        Reference r3 = new Reference("Helaman", 5, 12, 13);
+        Reference r3 = new Reference("Helaman", 5, 12);
         Scripture s1 = new Scripture(r3,"And now, my sons, remember, remember that it is upon the rock of our Redeemer, who is Christ, the Son of God, that ye must build your foundation; that when the devil shall send forth his mighty winds, yea, his shafts in the whirlwind, yea, when all his hail and his mighty storm shall beat upon you, it shall have no power over you to drag you down to the gulf of misery and endless wo, because of the rock upon which ye are built, which is a sure foundation, a foundation whereon if men build they cannot fall.");
 
         string option = "";
