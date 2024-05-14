b006228ce58096a2321ae86bf389539f3af1da52
thandosim <tdmpembe@live.com>
2024-05-14 22:27:10 +0200

    finished w4 learning activities

---
+++ prepare/Learning04/MathAssignment.cs
@@ -0,0 +1,17 @@
+public class MathAssignment : Assignment
+{
+    private string _textbookSection;
+    private string _problems;
+
+    public MathAssignment(string name, string topic, string textbookSection, string problems) 
+        : base (name,topic)
+    {
+        _textbookSection = textbookSection;
+        _problems = problems;
+    }
+
+    public string GetHomeworkList()
+    {
+        return $"Section {_textbookSection} Problems {_problems}";
+    }
+}
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        // Console.WriteLine("Hello Learning04 World!");
+        Assignment assign1 = new Assignment("Samuel Bennet", "Multiplication");
+        Console.WriteLine(assign1.GetSummary());
+
+        MathAssignment assign2 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-9");
+        Console.WriteLine(assign2.GetSummary());
+        Console.WriteLine(assign2.GetHomeworkList());
+
+        WritingAssignment assign3 = new WritingAssignment("Mary Waters", "Europian History", "The Causes of World War II");
+        Console.WriteLine(assign3.GetSummary());
+        Console.WriteLine(assign3.GetWritingInformation());
     }
 }
