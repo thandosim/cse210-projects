3cdf2f46bb85ef7345743c8992bc7d01800d3c4a
thandosim <tdmpembe@live.com>
2024-05-17 21:17:09 +0200

    completed listing activity fully

--- prove/Develop04/ReflectingActivity.cs
+++ prove/Develop04/ReflectingActivity.cs
@@ -1,6 +1,6 @@
 public class ReflectingActivity : Activity
 {
-    private List<string> _questions;
+    private List<string> _questions = new List<string>();
     private List<string> _prompts = new List<string>();
 
     public ReflectingActivity()
@@ -12,28 +12,32 @@ public class ReflectingActivity : Activity
     public void Run()
     {
         DisplayStartingMessage();
+        Console.Clear();
+        Console.WriteLine("Get ready...");
         ShowSpinner(5);
+        Console.WriteLine();
+        Console.WriteLine("Consider the following prompt:");
+        Console.WriteLine();
         DisplayPrompt();
-        ShowSpinner(5);
-        DisplayQuestions();
-        ShowSpinner(5);
-        DisplayQuestions();
-        ShowSpinner(5);
-        DisplayQuestions();
-        ShowSpinner(5);
-        DisplayEndingMessage(); 
+        Console.WriteLine();
+        Console.WriteLine("When you have something in mind, press enter to continue.");
+        ConsoleKey conK = Console.ReadKey(true).Key;
+        Console.WriteLine("Now ponder on each of the following questions as they relate to this experience.");
+        Console.Write("You may begin in: ");
+        ShowCountDown(2);        
+        Console.Clear();
+
+        DateTime startTime = DateTime.Now;
+        DateTime futureTime = startTime.AddSeconds(_duration);
+        DateTime currentTime = DateTime.Now;
+        while (currentTime < futureTime)
+        {
+            DisplayQuestions();
+            ShowSpinner(5);
+            currentTime = DateTime.Now;
+        }
+        DisplayEndingMessage();
     }
-// consider for extra credit
-//     ConsoleKey conK = Console.ReadKey(true).Key;
-// while (!Console.KeyAvailable && conK != ConsoleKey.L)
-// {
-//     if (conK == ConsoleKey.N)
-//     {
-//         // Do something when 'N' key is pressed
-//         Console.WriteLine("N key pressed.");
-//     }
-//     conK = Console.ReadKey(true).Key;
-// }
 
     public string GetRandomPrompt()
     {
@@ -42,24 +46,35 @@ public class ReflectingActivity : Activity
         _prompts.Add("Think of a time when you helped someone in need.");
         _prompts.Add("Think of a time when you did something truly selfless.");
         Random randomGenerator = new Random();
-        int number = randomGenerator.Next(0,4);
+        int number = randomGenerator.Next(0,3);
 
         return _prompts[number];
     }
 
     public string GetRandomQuestion()
     {
-        return "Question 1";
+        _questions.Add("Why was this experience meaningful to you?");
+        _questions.Add("Have you ever done anything like this before?");
+        _questions.Add("How did you get started?");
+        _questions.Add("How did you feel when it was complete?");
+        _questions.Add("What made this time different than other times when you were not as successful?");
+        _questions.Add("What is your favorite thing about this experience?");
+        _questions.Add("What could you learn from this experience that applies to other situations?");
+        _questions.Add("What did you learn about yourself through this experience?");
+        _questions.Add("How can you keep this experience in mind in the future?"); 
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(0,9);
+        return _questions[number];
     }
 
     public void DisplayPrompt()
     {
-        Console.WriteLine(GetRandomPrompt());
+        Console.WriteLine($"--- {GetRandomPrompt()} ---");
     }
 
     public void DisplayQuestions()
     {
-        Console.WriteLine(GetRandomQuestion());        
+        Console.WriteLine($"> {GetRandomQuestion()}");        
     }
 
 
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,18 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+
+        ConsoleKey conK = Console.ReadKey(true).Key;
+        while (!Console.KeyAvailable && conK != ConsoleKey.L)
+        {
+            // if (conK == ConsoleKey.N)
+            // {
+            //     // Do something when 'N' key is pressed
+            //     Console.WriteLine("N key pressed.");
+            // }
+            // conK = Console.ReadKey(true).Key;
+        }
+        // Do something after 'L' key is pressed and the loop breaks
+
     }
 }
