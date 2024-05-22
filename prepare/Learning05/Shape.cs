c954dc4464a1efcebb1e4478c2ff62231f0abf38
thandosim <tdmpembe@live.com>
2024-05-22 13:06:47 +0200

    step 1 to 3 runs well, some stub created

---
+++ prepare/Learning05/Shape.cs
@@ -0,0 +1,19 @@
+public class Shape
+{
+    string _color;
+
+    public string GetColor()
+    {
+        return "";
+    }
+
+    public void SetColor(string color)
+    {
+
+    }
+
+    public virtual double GetArea()
+    {
+        return 0;
+    }
+}
