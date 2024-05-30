224d80f60cd8eabaa524bebf32f095583de28c17
thandosim <tdmpembe@live.com>
2024-05-30 11:12:02 +0200

    created stub code for f1

---
+++ final/Foundation1/Comment.cs
@@ -0,0 +1,10 @@
+public class Comment
+{
+    private string _name;
+    private string _text;
+
+    public string GetCommentString()
+    {
+        return "";
+    }
+}
---
+++ final/Foundation1/Video.cs
@@ -0,0 +1,22 @@
+public class Video
+{
+    private string _title;
+    private string _author;
+    private float _length;
+    private List<Comment> _comments;
+
+    public Video()
+    {
+
+    }
+
+    public void Display()
+    {
+
+    }
+
+    public int GetCommentAmmount()
+    {
+        return 0;
+    }
+}
